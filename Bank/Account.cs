32e26b7621d61d85934d1c10e8a2e7c24ee69662
ahmad aljwaed <ahmadaljawaed@gmail.com>
2024-09-19 19:28:24 +0200

    Projektdateien hinzufügen.

---
+++ Bank/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Account
+    {
+
+        public int AccNo { get;  }
+        public String Owner  { get;  }
+        public Double Balance { get; private set; }
+
+
+
+        public Account(int accNo,String owner,Double balance) { 
+
+            AccNo = accNo;
+             Owner = owner;
+            Balance = balance;
+        
+        }
+
+        public Account()
+        {
+        }
+
+        public void makeDeposit(int amounth)
+        {
+            this.Balance+= amounth;
+        }
+
+        public void makeWithdeow(int amount)
+        {
+            if (this.Balance == amount || Balance > amount) { this.Balance -= amount; }
+            else { Console.WriteLine(" nicht genug Geld da"); }
+        }
+
+
+
+        public void getBalance() {
+            Console.WriteLine($" Kontostand von {Owner}: {Balance}");
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+using Bank;
+
+Account ac = new Account(12,"ahmad",122.3);
+
+while (true)
+{
+    Console.WriteLine(  "1 wähle eine aktion aus kontostand anzeigen");
+    Console.WriteLine("2 wähle eine aktion aus Einzahlung");
+
+    Console.WriteLine("3 wähle eine aktion aus Auszahlung");
+    String userEingabe= Console.ReadLine();
+   
+     if (userEingabe == "1")
+    {
+        Console.WriteLine(ac.Balance);
+    }
+    else if (userEingabe == "2")
+    {
+        Console.WriteLine(  " wie viel möchten Sie einzahlen");
+        int betrag=int.Parse(Console.ReadLine());
+        ac.makeDeposit(betrag);
+        Console.WriteLine( ac.Balance);
+    }
+
+    else if (userEingabe == "3")
+    {
+        Console.WriteLine(" wie viel möchten Sie auszahlen");
+        int betrag = int.Parse(Console.ReadLine());
+        ac.makeWithdeow(betrag);
+        Console.WriteLine(ac.Balance);
+    }
+    else
+    {
+        Console.WriteLine(  " falsche eingabe ");
+        break;
+    }
+
+
+
+
+}
